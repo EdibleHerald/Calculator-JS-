09b1c70b4ce0e4e2b4d5303bd3fed453dfa59c5d
EdibleHerald <142949302+EdibleHerald@users.noreply.github.com>
2024-06-29 14:24:43 -0400

    Added display functionality

--- script.js
+++ script.js
@@ -1,48 +1,148 @@
 // Create an interactive calculator!
 // For right now, focus on creating the essential functions!
+let result;
+let eachOper = '+-/*';
 
 function add(num1,num2){
-    return num1+num2;
+    
+    result = parseInt(num1)+parseInt(num2);
+    console.log(result);
+
+
+    return result;
 }
 
 function subtract(num1,num2){
-    return num1-num2;
+    result = parseInt(num1)-parseInt(num2);
+    console.log(result);
+
+
+    return result;
 }
 
 function multiply(num1,num2){
-    return num1*num2;
+    result = parseInt(num1)*parseInt(num2);
+    console.log(result);
+
+
+    return result;
 }
 
 function divide(num1,num2){
-    return num1/num2;
+    result = parseInt(num1)/parseInt(num2);
+    console.log(result);
+    floatResult = parseFloat(result)
+
+    return floatResult.toFixed(4);
 }
 
 // Create variables for operator, num1, num2;
-let num1, num2, oper;
+let num1 = '';
+let num2 = '';
+let oper = '';
 
 // Create function Operate which calls on one of the operator functions
 
-function operate(num1,num2,oper){
-    switch (oper) {
-        case "addition":
-            add(num1,num2);
-        case "subtraction":
-            subtract(num1,num2);
-        case "multiplication":
-            multiply(num1,num2);
-        case "division":
-            divide(num1,num2);
+// in order to test which number variable the input should be assigned to:
+// I create an array with all the operators used
+// Then I use .include 
+
+let operators = ["+","-","/","*"]
+
+// Create a variable that holds what is supposed to be on the display
+let displayValue='';
+
+function display(arg){
+// Easy access to display textbox
+let displayBox = document.querySelector(".display");
+
+displayValue+=arg;
+
+
+// CASE 1: NUMBER
+if (typeof(arg) == 'number'){
+    let stringArg = arg.toString();
+ // What if its the first number?
+ // Create an array testing for each operator in the display box
+    let testForOper = operators.map((operator) => {
+        return displayValue.includes(operator); // Reads whats in the display textbox and checks for any operators
+    })
+    // if one is true, then the input must be the second number
+    if(testForOper.includes(true)){
+        num2 += arg;
+        displayBox.textContent += arg;
+    }else{
+        num1 += arg;
+        displayBox.textContent += arg;
     }
+
+// CASE 2: OPERATION
+
+} else if(typeof(arg) == "string" && arg != "=" && arg != 'clear'){
+    switch(arg){
+        // classifies operation
+        case '+':
+        oper = '+';
+        break;
+        case '-':
+        oper = '-';
+        break;
+        case '/':
+        oper = '/';
+        break;
+        case '*':
+        oper = '*';
+        break;
+    }
+
+    displayBox.textContent += arg;
+    
+    let text = displayBox.textContent;
+    let textArray = text.split("");
+    let mappedText = textArray.map((item)=>eachOper.includes(item));
+    let filteredText = mappedText.filter((item)=>{return item});
+
+    if(filteredText.length > 1){
+        display('=');
+
+    }
+
+    
+
 }
 
-// Create a display function which displays the chosen option
+// CASE 3: EQUAL SIGN
 
-function display(variable){
-if(typeof variable == 'number' ){
-    document.querySelector(".display").textContent += variable;
-} else if (variable == 'clear'){
-    document.querySelector(".display").textContent = '';
-} else{
-    document.querySelector(".display").textContent +=variable;
+
+else if(arg == '='){
+
+    switch(oper){
+        case '+':
+            add(num1,num2);
+            break;
+        case '-':
+            subtract(num1,num2);
+            break;
+        case '/':
+            divide(num1,num2);
+            break;
+        case '*':
+            multiply(num1,num2);
+            break;
+    }
+    displayBox.textContent = result;
+    num1 = result;
+    num2 = '';
+    oper = '';
+    result = '';
+    
+}
+
+// CASE 4: CLEAR
+else if(arg == 'clear'){
+    displayBox.textContent = '';
+    oper = '';
+    num1 = '';
+    num2 = '';
 }
 }
